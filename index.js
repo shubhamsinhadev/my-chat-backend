c2a601b7a83ec0f55986b633830e67997d05a1a1
shubhamsinhadev <147917207+shubhamsinhadev@users.noreply.github.com>
2024-02-21 13:18:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,15 +8,24 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://my-chat-app-lemon-eight.vercel.app/",
+    origin: "https://my-chat-app-lemon-eight.vercel.app",
   },
 });
 
 app.use(
   cors({
-    origin: "https://my-chat-app-lemon-eight.vercel.app/",
+    origin: "https://my-chat-app-lemon-eight.vercel.app",
   })
 );
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://my-chat-app-lemon-eight.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 app.get("/", (req, res) => {
   res.send('Hello World');
