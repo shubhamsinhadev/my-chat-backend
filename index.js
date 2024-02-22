9ca5c462ff5346fd20ab4bf288a2a4a79cdbe505
shubhamsinhadev <147917207+shubhamsinhadev@users.noreply.github.com>
2024-02-22 13:18:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,11 +20,14 @@ app.use(
 
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://my-chat-app-lemon-eight.vercel.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', true);
+  const allowedOrigin = "https://my-chat-app-lemon-eight.vercel.app";
+  const origin = req.headers.origin || req.headers.referer;
+
+  if (origin === allowedOrigin) {
     next();
+  } else {
+    res.status(403).send('Forbidden'); // Block the request
+  }
 });
 
 app.get("/", (req, res) => {
